ea2bf685efd806125fb842584f35c859dd290360
tlekeyev-t <tlekeyev.temirlan@gmail.comm>
2022-08-05 18:57:10 +0600

    Responsive design

--- src/Components/Main/Mainpage.js
+++ src/Components/Main/Mainpage.js
@@ -70,55 +70,7 @@ const Main = () => {
             </div>
         </div>
 
-    //     <div className="wrapper">
-    //     <div className="textcontainer">
-    //         <h1 className="h1">Nomad location</h1>
-            
-    //         <h3 className="h3">Берілген суреттер арқылы Алматы, Нұр-Сұлтан және бүкіл Қазақстан ішіндегі түкпір-түкпіріндегі әйгілі жерлерді картадан табыңыз!
-    //         <br/>Қазақстандағы туризмді дамыту. <br/>
-    //              Туристерге және шетел елдер қонақтарға көрсету.</h3>
-    //     </div>
-
-
-    //         <div className="destination">
-    //         <h1 className="h2">Баратын жеріңізді таңдаңыз:</h1>
-    //         </div>
-    //     <div className="levelcontainer">
-    
-    //         <div className="level">
-    //             <div className="description">
-    //             <h2 className="h2name">Алматы</h2>
-    //             <h3 className="desc">Алматылықтардың ең көп баратын және сүйікті 30 жерін тап!
-    //             </h3>
-    //             <img className="mainimg" src={ala}/>
-    //             <div className="button">
-    //                 <button className="btn" onClick={()=>{navigate('/Almaty')}}><FaPlay/> Play</button>
-    //             </div>
-    //             </div>
-    //         </div>
-
-    //         <div className="level">
-    //             <div className="description">
-    //             <h2 className="h2name">Нұр-Сұлтан</h2>
-    //             <h3 className="desc">Нұр-Сұлтандықтардың ең көп баратын және сүйікті 30 жерін тап!
-    //             </h3>
-    //             <img className="mainimg" src={tse}/>
-    //             <div className="button"><button className="btn" onClick={()=>{navigate('/Nursultan')}}><FaPlay/> Play</button></div> 
-    //             </div>
-    //         </div>
-
-    //         <div className="level">
-    //             <div className="description">
-    //             <h2 className="h2name">Қазақстан</h2>
-    //             <h3 className="desc">Қазақстан тұрғындары мен әлемнің түкпір-түкпірінен келетін <br/> туристертердің ең көп баратын және сүйікті жерлерін табыңыз!</h3>
-    //             <img className="mainimg" src={kaz}/>
-    //             <div className="button">
-    //                 <button className="btn" onClick={()=>{navigate('/Kazakhstan')}}><FaPlay/> Play</button>
-    //             </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+ 
     )
 }
 
